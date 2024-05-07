be8e9dd365b287c1cfbe4884a0af63e38de886f9
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2024-05-07 15:37:52 +0200

    omit left side graphic on small screens

--- template-glitzer-left.php
+++ template-glitzer-left.php
@@ -15,7 +15,7 @@ get_header(); ?>
 	<div class="content">
 		<div class="grid-container">
 			<div class="inner-content grid-x grid-margin-x grid-padding-x">
-				<div id="glitzer-left" class="medium-4 large-4 cell">
+				<div id="glitzer-left" class="medium-4 large-4 cell show-for-large">
 					<div class="grid-x" style="height:100%">
 						<div class="cell medium-2 large-2"></div>
 						<div class="cell medium-4 large-4">
